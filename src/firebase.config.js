04f9506f16fc1c01f87f55a93046cf575c10bea1
Minjur <minjur.rahaman99@gmail.com>
2022-10-27 12:35:52 +0600

    config file edit with env file

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDOTfTfAfw4UuUIIAO_cquQjsx4LZBgdPk",
-  authDomain: "learning-house-b3787.firebaseapp.com",
-  projectId: "learning-house-b3787",
-  storageBucket: "learning-house-b3787.appspot.com",
-  messagingSenderId: "1062066942171",
-  appId: "1:1062066942171:web:34518cd9bfd2b94b471cfe"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
